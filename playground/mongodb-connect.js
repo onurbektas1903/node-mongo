023294011ae6aa2dcdee2da60f246ee5de9daba3
Onuralp <bektaso1903@gmail.com>
2018-11-17 15:25:50 +0300

    all coommit

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,39 +1,35 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient,ObjectID} = require('mongodb');
-var obj = new ObjectID();
+const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true },(err,client) => {
-    if (err) {
-        return console.log('Unable to connect mongo server');
-    }
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    var user = {name:'onur',age:25};
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
-    var {name} = user;
-    console.log('Connected MongoDb server');
-    // const db = client.db('TodoApp');
-    // db.collection('Todos').insertOne({
-    //     text:'Something to do 2',
-    //     completed:false
-    // },(err,result)=>{
-    //    if(err){
-    //        return console.log('Unable to insert todo');
-    //    }
-    //    console.log(JSON.stringify(result.ops));
-    // });
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
-    const db = client.db('TodoApp');
-    db.collection('Users').insertOne({
-        name: 'Onur',
-        age: 28,
-        location: 'Ankara'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert user');
-        }
-        console.log(JSON.stringify(result.ops));
-
-    });
-
-    client.close();
+  db.close();
 });
